9a895b2cf9f39bf924e8de99f3b6c180c97ebc85
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-01-31 08:33:32 +0800

    Cランク獲得 標準出力: 【文字列の出力】入力された 10 個の文字列を出力

--- 0_stdout_primer/03.js
+++ 0_stdout_primer/03.js
@@ -4,33 +4,52 @@
   /*
     【文字列の出力】入力された 10 個の文字列を出力
     practiceDate: 20240131
-    setp01: 
-    step02: 
-    step03: 
-    step04: 
-    boss: 
+    setp01: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step1
+    step02: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step2
+    step03: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step3
+    step04: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_step4
+    boss: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__string_output_boss
   */
 
  //  const lines = [];
 
   step01();
   function step01() {
+    console.log("paiza");
   }
   
   step02();
   function step02() {
-
+    const input = "paiza learning";
+    const lines = input.split(" ");
+    console.log(lines.join(" "));
   }
 
   step03();
   function step03() {
+    const lines = ['paiza', 'learning'];
+    for (const line of lines) {
+      console.log(line);
+    }
   }
 
   step04();
   function step04() {
+    const lines = [
+      'q', 'bpdi', 'u',
+      'ky', 'meqt', 'rrnc',
+      'co', 'jjw', 'e',
+      'fwio'
+    ];
+    console.log(lines.join(" "));
   }
 
   boss();
   function boss() {
+    const lines = [ 'a a a a a a a a a a' ];
+    const inputs = lines[0].split(" ");
+    for (const input of inputs) {
+      console.log(input);
+    }
   }
 }
---
+++ 0_stdout_primer/04.js
@@ -0,0 +1,36 @@
+"use strict";
+
+{
+  /*
+    【n 行の出力】1,000 行以内の出力
+    practiceDate: 20240130
+    setp01: 
+    step02: 
+    step03: 
+    step04: 
+    boss: 
+  */
+
+ //  const lines = [];
+
+  step01();
+  function step01() {
+  }
+  
+  step02();
+  function step02() {
+
+  }
+
+  step03();
+  function step03() {
+  }
+
+  step04();
+  function step04() {
+  }
+
+  boss();
+  function boss() {
+  }
+}
