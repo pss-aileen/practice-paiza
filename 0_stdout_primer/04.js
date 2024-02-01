087b90a42a430e5362bcad3d716055c1f323e4e2
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-01 09:51:10 +0800

    Cランク獲得 標準出力: 【n 行の出力】1,000 行以内の出力

--- 0_stdout_primer/04.js
+++ 0_stdout_primer/04.js
@@ -3,34 +3,44 @@
 {
   /*
     【n 行の出力】1,000 行以内の出力
-    practiceDate: 20240130
-    setp01: 
-    step02: 
-    step03: 
-    step04: 
-    boss: 
+    practiceDate: 20240201
+    setp01: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step1
+    step02: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step2
+    step03: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_step3
+    boss: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__n_line_boss
   */
 
  //  const lines = [];
 
-  step01();
+  // step01();
   function step01() {
+    const lines = ['2'];
+    for (let i = 1; i <= Number(lines[0]); i++) {
+      console.log(i);
+    }
   }
   
-  step02();
+  // step02();
   function step02() {
-
+    const lines = ['5'];
+    for (let i = 1; i <= Number(lines[0]); i++) {
+      console.log(i);
+    }
   }
 
-  step03();
+  // step03();
   function step03() {
-  }
-
-  step04();
-  function step04() {
+    const lines = ['10'];
+    for (let i = 1; i <= Number(lines[0]); i++) {
+      console.log(i);
+    }
   }
 
   boss();
   function boss() {
+    const lines = ['1000'];
+    for (let i = 1; i <= Number(lines[0]); i++) {
+      console.log(i);
+    }
   }
 }
---
+++ 0_stdout_primer/05.js
@@ -0,0 +1,37 @@
+"use strict";
+
+{
+  /*
+    https://paiza.jp/works/mondai/stdout_primer/problem_index?language_uid=javascript
+    【特定の文字で区切り 1 行で出力】大きな数値を 3 けたごとにカンマ区切りで出力
+    practiceDate: 20240130
+    setp01: 
+    step02: 
+    step03: 
+    step04: 
+    boss: 
+  */
+
+ //  const lines = [];
+
+  step01();
+  function step01() {
+  }
+  
+  step02();
+  function step02() {
+
+  }
+
+  step03();
+  function step03() {
+  }
+
+  step04();
+  function step04() {
+  }
+
+  boss();
+  function boss() {
+  }
+}
