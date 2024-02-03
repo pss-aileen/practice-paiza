9aa82aa9f6d76cd8b05c12b6c5157c43285574c8
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-03 11:45:37 +0800

    Cランク獲得 標準出力: 【n * n の 2 次元配列の表示】N * N の九九表の出力

--- 0_stdout_primer/06.js
+++ 0_stdout_primer/06.js
@@ -3,32 +3,68 @@
 {
   /*
     【n * n の 2 次元配列の表示】N * N の九九表の出力
-    practiceDate: 2024XXXX
+    practiceDate: 20240203
     url: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__2dim_array_step1
-    time: 
-    thoughts: 
+    time: 35min
+    thoughts: すこし難しかったけど思いの外できた。ループで i++ 以外（i = i + 3）を使ったのははじめてでした...
   */
 
  //  const lines = [];
 
   step01();
   function step01() {
+    const input = [0, 8, 1, 3];
+    let formattedText = "";
+    for (let i = 0; i < input.length; i++) {
+      if ((i + 1) % 2 === 0 && i + 1 !== input.length) {
+        formattedText = formattedText + input[i] + "\n";
+      } else if (i + 1 === input.length) {
+        formattedText = formattedText + input[i];
+      } else {
+        formattedText = formattedText + input[i] + " ";
+      }
+    }
+    console.log(formattedText);
   }
   
   step02();
   function step02() {
+    // const lines = ['0 1 2 3 4 5 6 7 8'];
+    const lines = ['11 12 13 14 15 16 17 18 19'];
+    const splitValues = lines[0].split(" ");
+    const formattedText = [];
+    
+    for(let i = 0; i < splitValues.length; i = i + 3) {
+      const oneLine = `${splitValues[i]} ${splitValues[i + 1]} ${splitValues[i + 2]}`;
+      formattedText.push(oneLine);
+    }
 
+    console.log(formattedText.join("\n"));
   }
 
   step03();
   function step03() {
-  }
-
-  step04();
-  function step04() {
+    for (let i = 1; i <= 9; i++) {
+      const oneline = [];
+      for (let j = 1; j <= 9; j++) {
+        const num = i * j;
+        oneline.push(num);
+      }
+      console.log(oneline.join(" "));
+    }
   }
 
   boss();
   function boss() {
+    const lines = [10];
+    const inputNumber = lines[0];
+    for (let i = 1; i <= inputNumber; i++) {
+      const oneline = [];
+      for (let j = 1; j <= inputNumber; j++) {
+        const num = i * j;
+        oneline.push(num);
+      }
+      console.log(oneline.join(" "));
+    }
   }
 }
---
+++ 0_stdout_primer/07.js
@@ -0,0 +1,34 @@
+"use strict";
+
+{
+  /*
+    【文字列の出力】入力された 10 個の文字列を出力
+    practiceDate: 2024XXXX
+    url: https://paiza.jp/works/mondai/stdout_primer/stdout_primer__variable_array_step1
+    time: 
+    thoughts: 
+  */
+
+ //  const lines = [];
+
+  step01();
+  function step01() {
+  }
+  
+  step02();
+  function step02() {
+
+  }
+
+  step03();
+  function step03() {
+  }
+
+  step04();
+  function step04() {
+  }
+
+  boss();
+  function boss() {
+  }
+}
